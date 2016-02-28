ef14a0e24366c3b72ff9c67336ab008f708ea4c8
Romain Piel <romain.piel@gmail.com>
2016-02-28 16:28:21 +0000

    added test updateSpans_singleSymbolBrush_twoMatches_alreadyOneMatch

--- app/src/test/kotlin/com/romainpiel/darktown/BrushTest.kt
+++ app/src/test/kotlin/com/romainpiel/darktown/BrushTest.kt
@@ -87,4 +87,19 @@ class BrushTest {
         assertThat(text.getSpanStart(spans[0])).isEqualTo(9)
         assertThat(text.getSpanEnd(spans[0])).isEqualTo(13)
     }
+
+    @Test
+    fun updateSpans_singleSymbolBrush_twoMatches_alreadyOneMatch() {
+        val brush = YoloBrush()
+        val text = SpannableStringBuilder("hey mate yolo hashtag it yolo!")
+        text.setSpan(YoloSpan(), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        brush.updateSpans(text, 0, text.length)
+
+        val spans = text.getSpans(0, text.length, YoloSpan::class.java)
+        assertThat(spans).hasLength(2)
+        assertThat(text.getSpanStart(spans[0])).isEqualTo(9)
+        assertThat(text.getSpanEnd(spans[0])).isEqualTo(13)
+        assertThat(text.getSpanStart(spans[1])).isEqualTo(25)
+        assertThat(text.getSpanEnd(spans[1])).isEqualTo(29)
+    }
 }
